ec52d57eb1db4df0e89715006cf853eb11b15e7f
AntonioStavrakev <antonio.stavrakev1@gmail.com>
2025-11-24 10:25:19 +0200

    + Completed Core

---
+++ EventManagement.Core/DTOs/RegistrationDTO.cs
@@ -0,0 +1,29 @@
+﻿namespace EventManagement.Core.DTOs
+{
+    public class RegistrationBaseDto
+    {
+        public DateTime RegistrationDate { get; set; }
+    }
+
+    public class RegistrationCreateDto : RegistrationBaseDto
+    {
+        public int UserId { get; set; }
+        public int EventId { get; set; }
+    }
+
+    public class RegistrationUpdateDto : RegistrationBaseDto
+    {
+        public int RegistrationId { get; set; }
+        public int UserId { get; set; }
+        public int EventId { get; set; }
+    }
+
+    public class RegistrationResponseDto : RegistrationBaseDto
+    {
+        public int RegistrationId { get; set; }
+        public int UserId { get; set; }
+        public int EventId { get; set; }
+        public string? UserName { get; set; }
+        public string? EventTitle { get; set; }
+    }
+}
