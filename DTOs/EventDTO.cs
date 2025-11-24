4b6fba95969eeb9080fdcab93c88f58b2911a2e7
GalinPatazov <119655761+GalinPatazov@users.noreply.github.com>
2025-11-24 10:22:11 +0200

    Add files via upload

---
+++ DTOs/EventDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagement.Core.DTOs
+{
+    public class EventBaseDto
+    {
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime Date { get; set; }
+        public string? Location { get; set; }
+    }
+
+    public class EventCreateDto : EventBaseDto
+    {
+        public int SpeakerId { get; set; }
+    }
+
+    public class EventUpdateDto : EventBaseDto
+    {
+        public int EventId { get; set; }
+        public int SpeakerId { get; set; }
+    }
+
+    public class EventResponseDto : EventBaseDto
+    {
+        public int EventId { get; set; }
+        public int SpeakerId { get; set; }
+        public string SpeakerName { get; set; }
+    }
+}
---
+++ DTOs/UserDTO.cs
@@ -0,0 +1,24 @@
+﻿namespace EventManagement.Core.DTOs
+{
+    public class UserBaseDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+
+    public class UserCreateDto : UserBaseDto
+    {
+    }
+
+    public class UserUpdateDto : UserBaseDto
+    {
+        public int UserId { get; set; }
+    }
+
+    public class UserResponseDto : UserBaseDto
+    {
+        public int UserId { get; set; }
+    }
+}
